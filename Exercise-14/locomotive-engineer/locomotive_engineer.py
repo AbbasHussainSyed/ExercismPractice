5360899dc8ea1fd334a50ac9b9e2629881d1ae49
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2023-12-31 00:40:35 -0800

    My 14th-Lesson with more successfull test cases in locomotive_engineer.py.py

--- Exercise-14/locomotive-engineer/locomotive_engineer.py
+++ Exercise-14/locomotive-engineer/locomotive_engineer.py
@@ -40,15 +40,11 @@ def extend_route_information(route, more_route_information):
     """
     return {**route, **more_route_information}
 
-def fix_wagon_depot(*wagons_rows):
+def fix_wagon_depot(wagons_rows):
     """Fix the list of rows of wagons.
 
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    for row in wagons_rows:
-        if row[0] not in row[1]:
-            row[0], row[1] = row[1], row[0]
-        else:
-            row[1] = row[1]
-    return wagons_rows
+    [*first_row], [*second_row], [*third_row] = zip(*wagons_rows)
+    return [first_row, second_row, third_row]
