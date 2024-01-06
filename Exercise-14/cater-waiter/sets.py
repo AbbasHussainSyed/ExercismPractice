75f9704de66b08f91ef7063ded18957ad2ee1287
Abbas Syed <86834814+AbbasHussainSyed@users.noreply.github.com>
2024-01-05 18:54:05 -0800

    My 14th lesson sets.py

--- Exercise-14/cater-waiter/sets.py
+++ Exercise-14/cater-waiter/sets.py
@@ -89,7 +89,7 @@ def separate_appetizers(dishes, appetizers):
     The function should return the list of dish names with appetizer names removed.
     Either list could contain duplicates and may require de-duping.
     """
-    result= set(dishes).difference(set(appetizers))
+    result = set(dishes).difference(set(appetizers))
     return list(result)
 
 def singleton_ingredients(dishes, intersection):
